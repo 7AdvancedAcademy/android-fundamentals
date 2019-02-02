900d18b8b86b5d9546e6d7a421c5035ed289b616
Masters <masters@Ms-Pro-18.local>
2019-02-02 19:27:03 +0100

    Udpated HelloConstraint Ap and added ClassLabs

--- Assignments/HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ Assignments/HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -1,20 +1,63 @@
 package com.example.helloconstraint;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button count = findViewById(R.id.count_button);
+        number = findViewById(R.id.number_field);
+        number.setText("0");
+
+        count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence valueChar = number.getText();
+                int valueInt = Integer.parseInt(valueChar.toString());
+                valueInt++;
+                number.setText(String.valueOf(valueInt));
+
+            }
+        });
+
     }
 
     public void onClick(View v){
-        Button toast = findViewById(R.id.toast_button);
+
+        TextView storedNumber = findViewById(R.id.number_field);
+        CharSequence showNumber = storedNumber.getText().toString();
+        Toast.makeText(this,showNumber,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        outState.putString("saved_number",number.getText().toString());
+        super.onSaveInstanceState(outState);
+
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        String resumed_number = savedInstanceState.getString("saved_number");
+        number.setText(resumed_number);
+        Log.d("on restore",resumed_number);
 
     }
 }
---
+++ ClassLabs/ScrollingText/app/src/main/java/com/example/scrollingtext/DetailsActivity.java
@@ -0,0 +1,36 @@
+package com.example.scrollingtext;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    public void onClick(View v) {
+
+        Uri uri = Uri.parse("http://www.google.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+
+
+
+
+    }
+
+}
